b5fc313da9f4934594678c4b47c6596eaa81124d
Bill Carlson <bill@coacoas.net>
2013-11-02 23:47:02 -0400

    Added P37

--- src/main/scala/net/coacoas/euler/Problems31to40.scala
+++ src/main/scala/net/coacoas/euler/Problems31to40.scala
@@ -2,8 +2,8 @@ package net.coacoas.euler
 
 import scala.annotation.tailrec
 
-object P31 extends Problem { 
-  def countChange(amount: Int) = { 
+object P31 extends Problem {
+  def countChange(amount: Int) = {
     def loop(coins: Vector[Int], remaining: Int): Long = {
       if (remaining == 0) 1
       else if (coins.isEmpty) 0
@@ -12,40 +12,63 @@ object P31 extends Problem {
     }
     loop(Vector(200, 100, 50, 20, 10, 5, 2, 1), amount)
   }
-  
+
   override def run = countChange(200)
 }
 
-object P34 extends Problem { 
+object P34 extends Problem {
   def digits(n: Int) = n.toString.toList.map(_ - '0')
-  def fact(n: Int): BigInt = { 
+  def fact(n: Int): BigInt = {
     @tailrec
-    def iter(acc: BigInt, m: Int): BigInt = 
+    def iter(acc: BigInt, m: Int): BigInt =
       if (m == 0 || m == 1) acc
       else iter(acc * m, m - 1)
     iter(1, n)
   }
-  
+
   lazy val facts = (0 to 9).map(P34.fact(_)).zipWithIndex.map(_.swap).toMap
 
   override def run = (3 to 10000000).par.filter(x => digits(x).map(facts).sum == x).sum
 }
 
 object P35 extends Problem {
-  lazy val primes = Streams.primes.takeWhile (_ < 1000000).toSet
-  
+  lazy val primes = Streams.primes.takeWhile(_ < 1000000).toSet
+
   def rotate[T](xs: Seq[T])(n: Int) = xs.slice(n, xs.length) ++ xs.slice(0, n)
-  	
+
   def circulars(n: Long): Seq[Long] = {
     val digits = P34.digits(n.toInt)
     (0 until digits.size).map(rotate(digits)(_).mkString.toLong)
   }
-  
+
   override def run = primes.map(P35.circulars).filter(_.tail.forall(primes)).size
 }
 
-object P36 extends Problem { 
+object P36 extends Problem {
   def isPalindrome(s: String) = s == s.reverse
-  
+
   override def run = (0L until 1000000L).filter(x => isPalindrome(x.toString)).filter(y => isPalindrome(y.toBinaryString)).sum
+}
+
+object P37 extends Problem {
+  lazy val primeSet = Streams.primes.takeWhile(_< 10000000).toSet
+  def isPrime(n: Long) = primeSet.contains(n)
+
+  def truncateLeft(n: Long) = n.toString.toList.tail.mkString.toLong
+  def truncateRight(n: Long): Long = n / 10
+
+  def isTruncatable(n: Long, f: Long => Long) = {
+    @tailrec
+    def iter(remaining: Long): Boolean = 
+      if (remaining < 10) isPrime(remaining)
+      else isPrime(remaining) && iter(f(remaining))
+
+    iter(n)
+  }
+
+  override def run = 
+    Streams.primes.dropWhile(_ < 10).
+      filter(isTruncatable(_, truncateLeft)).
+      filter(isTruncatable(_, truncateRight)).
+      take(11).sum
 }
